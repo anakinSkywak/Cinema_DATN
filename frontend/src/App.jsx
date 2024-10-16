efb90156e1935c59513b59e24b36f7c96b2995da
[do bang] <bangdtph32046@fpt.edu.vn>
2024-10-16 11:29:02 +0700

    update base fe

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HistoryRouter } from './components/HistoryRouter';
+import routes from './routes';
+import PrivateRoute from './components/PrivateRoute';
+import NotFound from './components/NotFound';
+import myHistory from './utils/myHistory';
+import "./style.css";
+
+function App() {
+    return (
+  <></>
+    );
+}
+
+export default App;
