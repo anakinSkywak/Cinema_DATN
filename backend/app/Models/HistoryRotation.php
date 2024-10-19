715112483947c78649de62a8a4b86d58c8868c53
VuBon1202 <vuxuanbon12022004@gmail.com>
2024-10-19 11:01:29 +0700

    quay phan thuong

---
+++ backend/app/Models/HistoryRotation.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HistoryRotation extends Model
+{
+    use HasFactory;
+
+    protected $table = 'history_rotations';
+
+    protected $fillable = [
+        'user_id', 'vongquay_id', 'ket_qua', 'ngay_quay', 'trang_thai'
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+    public function rotation()
+    {
+        return $this->belongsTo(Rotation::class, 'vongquay_id');
+    }
+}
