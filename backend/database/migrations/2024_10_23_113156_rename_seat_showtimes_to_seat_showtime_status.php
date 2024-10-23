8624d0c6d1b2eb8ffa58a49332a9b0ec80a41dff
Aanhiu <buianh20003@gmail.com>
2024-10-23 18:32:59 +0700

    doi ten table seat_showtimes thanh seat_showtime_status

---
+++ backend/database/migrations/2024_10_23_113156_rename_seat_showtimes_to_seat_showtime_status.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::rename('seat_showtimes', 'seat_showtime_status');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('seat_showtime_status', function (Blueprint $table) {
+            //
+        });
+    }
+};
