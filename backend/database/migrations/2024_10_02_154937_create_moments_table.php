4d60e641983020768c9c9224089034af434a24aa
Tiến <150010724+tienne-1311@users.noreply.github.com>
2024-10-25 00:25:03 +0700

    Update 2024_10_02_154937_create_moments_table.php

--- backend/database/migrations/2024_10_02_154937_create_moments_table.php
+++ backend/database/migrations/2024_10_02_154937_create_moments_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('moments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->references('id')->on('users');
-            $table->foreignId('phim_id')->references('id')->on('movies');
-            $table->string('anh_khoang_khac' , 255);
-            $table->string('noi_dung' , 255);
-            $table->float('like');
-            $table->float('dislike');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('phim_id')->constrained('movies')->onDelete('cascade');
+            $table->string('anh_khoang_khac', 255)->nullable(); // Ảnh có thể nullable
+            $table->string('noi_dung', 255);
+            $table->integer('like')->default(0); // Số like mặc định là 0
+            $table->integer('dislike')->default(0); // Số dislike mặc định là 0
             $table->timestamps();
         });
     }
