fd1f92eb9cb9b6874fd82a5b86c0c5e6b7a19788
anakinSkywak <tratra1711@gmail.com>
2024-10-28 14:26:26 +0700

    lã hồng việt thêm model moment

---
+++ backend/app/Models/Moment.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Moment extends Model
+{
+    use HasFactory;
+
+    protected $table = 'moments';
+
+    protected $fillable = [
+        'user_id',
+        'phim_id',
+        'anh_khoang_khac',
+        'noi_dung',
+        'like',
+        'dislike'
+    ];
+
+    /**
+     * Get the user that owns the comment.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * Get the user that owns the Comment
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function movie(): BelongsTo
+    {
+        return $this->belongsTo(Movie::class, 'phim_id');
+    }
+
+}
