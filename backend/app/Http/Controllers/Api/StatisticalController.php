13bd0b5a0b7890f1c3d988675c397949b6798dc2
anakinSkywak <tratra1711@gmail.com>
2024-11-24 17:14:37 +0700

    thêm controller thông kê

---
+++ backend/app/Http/Controllers/Api/StatisticalController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+// chức năng thống kê
+class StatisticalController extends Controller
+{
+    //
+}
