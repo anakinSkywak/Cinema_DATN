78b9c7f5e681e3ddc3ae2258d224797a2335c876
tienne-1311 <tiendz13112004@gmail.com>
2024-11-27 16:24:59 +0700

    sửa lại (tạo khoảng khắc săn mã theo mã giảm giá coupons )

--- backend/app/Models/CountdownVoucher.php
+++ backend/app/Models/CountdownVoucher.php
@@ -18,18 +18,26 @@ class CountdownVoucher extends Model
         'so_luong_con_lai',
         'trang_thai',
     ];
+
+    // Sử dụng booted để xử lý sự kiện tạo đối tượng
     protected static function booted()
     {
-        static::creating(function ($coupons) {
-            if (is_null($coupons->so_luong_con_lai)) {
-                $coupons->so_luong_con_lai = $coupons->so_luong;
+        // Thêm sự kiện khi tạo mới CountdownVoucher
+        static::creating(function ($countdownVoucher) {
+            // Nếu số lượng còn lại chưa được xác định, gán nó bằng số lượng
+            if (is_null($countdownVoucher->so_luong_con_lai)) {
+                $countdownVoucher->so_luong_con_lai = $countdownVoucher->so_luong;
             }
         });
     }
-    public function coupons()
+
+    // Quan hệ với Coupon, mỗi CountdownVoucher thuộc về một Coupon
+    public function coupon()
     {
         return $this->belongsTo(Coupon::class, 'magiamgia_id');
     }
+
+    // Quan hệ với CouponCodeTaken, mỗi CountdownVoucher có nhiều CouponCodeTaken
     public function couponCodeTakens()
     {
         return $this->hasMany(CouponCodeTaken::class, 'countdownvoucher_id');
