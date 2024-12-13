9da8d3875338379a6294debd11afa98e6f7bbbc5
tienne-1311 <tiendz13112004@gmail.com>
2024-12-13 20:27:38 +0700

    update bảng coupons

---
+++ backend/database/migrations/2024_12_13_132700_add_so_luong_con_lai_to_coupons_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSoLuongConLaiToCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('coupons', function (Blueprint $table) {
+            $table->integer('so_luong_con_lai')->unsigned()->default(0)->after('so_luong'); // Thêm cột mới
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('coupons', function (Blueprint $table) {
+            $table->dropColumn('so_luong_con_lai'); // Xóa cột nếu rollback
+        });
+    }
+}
